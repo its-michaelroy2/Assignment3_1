c79be12f4a44c188e369a347f14929c6d18a94a1
its-michaelroy <72664547+its-michaelroy@users.noreply.github.com>
2024-11-18 10:40:04 -0500

    Begin Assignments

---
+++ Assignment3_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Assignment3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=================================");
+            Console.WriteLine("         Assignment3_1_1");
+            Console.WriteLine("=================================\n\n");
+            Console.WriteLine("1. Write a method that returns a string of even numbers greater than 0 and less than 100. Use the stringbuilder class.\n");
+
+            string evenNumbers = GetEvenNumbers();
+            Console.WriteLine("Even numbers from 1-100");
+            Console.WriteLine(evenNumbers);
+
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue...Assignment 3.2, etc");
+            Console.ReadLine();
+        }
+
+        static string GetEvenNumbers()
+        {
+            StringBuilder evenNumbers = new StringBuilder();
+            for (int i = 2; i <= 100; i += 2)
+            {
+                evenNumbers.Append(i);
+                if (i <= 98)
+                {
+                    evenNumbers.Append(", ");
+                }
+            }
+            return evenNumbers.ToString();
+        }
+    }
+}
